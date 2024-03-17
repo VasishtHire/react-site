41fdf1610058a96194dce0d6dd3cee9f3f714810
VazorMusic <vazormusic@gmail.com>
2024-03-18 00:06:09 +0530

    Third Commit

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 function App() {
   return (
     <>
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact />
+        <Route path='/' element={<Home/>}/>
         </Routes>
       </Router>
     </>
--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -1,15 +1,42 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+// import './css/style.css';
+// import './css/animate.css';
+// import './css/bootstrap.css';
+// import './css/bootstrap-datepicker.css';
+// import './css/aos.css';
+// import './css/fancybox.min.css';
+// import './css/flaticon.css';
+// import './css/icomoon.css';
+// import './css/jquery.timepicker.css';
+// import './css/magnific-popup.css';
+// import './css/open-iconic-bootstrap.min.css';
+// import './css/owl.carousel.min.css';
+// import './css/owl.theme.default.min.css';
+// import './css/ionicons.min.css';
 
 function Navbar() {
   return (
     <>
-    <nav className="Navbar">
-        <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-                Logo
-            </Link>
+    <nav className="nav">
+        <div className="container">
+            <div className="logo">
+                <a href="#">Your Logo</a>
+            </div>
+            <div id="mainListDiv" className="main_list">
+                <ul className="navlinks">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <span className="navTrigger">
+                <i></i>
+                <i></i>
+                <i></i>
+            </span>
         </div>
     </nav>
     </>  
